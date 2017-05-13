e40f851e552bd9f987ea77bc40a7c342ec806723
HENRY <henryscott325@gmail.com>
2017-05-12 21:50:22 -0600

    Initial commit

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
 import discord
-from AradiaCore import aradiaCore
+from AradiaCore import AradiaCore
 
 
-class bot(aradiaCore):
+class Bot(AradiaCore):
     def __init__(self):
         super().__init__()
-    @aradiaCore.bot_owner
-    async def cmd_owner(self,context):
 
+    @AradiaCore.bot_owner
+    async def cmd_owner(self,context):
         return await self.say('Hello, {context.author.name}. You are the bot owner!'.format(**locals()))
+
     async def cmd_test(self,context):
         em = discord.Embed(title='Links',colour=context.author.colour)
         em.add_field(name='Github',value='https://github.com/AggressivelyMeows/AradiaCore',inline=False)
         em.add_field(name='Website',value='http://aradiabot.me/')
+
         return await self.say("Hello, I am AradiaCore!\n**I am a bot framework made by Cerulean#7014 and made in python! Check out my links below!**",embed=em)
+
     async def cmd_meme(self,context):
         """
         Quick example command
@@ -21,6 +24,7 @@ class bot(aradiaCore):
         em = discord.Embed(title='Memes')
         em.add_field(name=context.server.name,value=context.author.name)
         return await self.say('',embed=em)
+
 if __name__ == '__main__':
-    aradiabot = bot()
+    aradiabot = Bot()
     aradiabot.boot()
